4ac26d329de512082647be00aaa417830171ca8b
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2021-09-15 20:37:36 -0400

    commit tree 718a46b2dba831a2d3aa436dcb305ab6cc8358eb

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // Your code here
 
+// let twoRows = [
+//     ["moe", "sizlak", "barkeep", 2],
+//     ["bartholomew", "simpson", "scamp", 3]
+//   ]
+
 function createEmployeeRecord(array) {
     return {
         firstName: array[0],
@@ -12,41 +17,89 @@ function createEmployeeRecord(array) {
 }
 
 function createEmployeeRecords(array) {
-    return array.map(createEmployeeRecord)
+    return array.map(arr => {
+        // debugger 
+        return createEmployeeRecord(arr)
+        })
 }
 
 function createTimeInEvent(record, date) {
-    
+     let [fullDate, hour] = date.split(" ")
+
     record.timeInEvents.push({
         type: "TimeIn",
+        hour: parseInt(hour),
+        date: fullDate,
         
     })
     return record
 }
 
 function createTimeOutEvent(record, date) {
+    let [fullDate, hour] = date.split(" ")
+
     record.timeOutEvents.push({
         type: "TimeOut",
+        hour: parseInt(hour),
+        date: fullDate,
     })
     return record
 }
 
-// function hoursWorkedOnDate(record, date) {
+// cRecord = createEmployeeRecord(["Julius", "Caesar", "General", 1000])
+// updatedBpRecord = createTimeInEvent(cRecord, "0044-03-15 0900")
+// updatedBpRecord = createTimeOutEvent(cRecord, "0044-03-15 1100")
 
-// }
+function hoursWorkedOnDate(record, date) {
+    let timeIn = record.timeInEvents.find(time => {
+        return time.date === date;
+    })
+    let timeOut = record.timeOutEvents.find(time => {
+        return time.date === date;
+    })
+    return (timeOut.hour - timeIn.hour) / 100
+    // debugger
+}
 
-// function wagesEarnedOnDate(record, date) {
+function wagesEarnedOnDate(record, date) {
+    let hoursWorked = hoursWorkedOnDate(record, date)
+    let payRate = record.payPerHour
 
-// }
+    return hoursWorked * payRate
+}
 
-// function allWagesFor(record) {
+cRecord = createEmployeeRecord(["Julius", "Caesar", "General", 27])
+        // Earns 324
+        updatedBpRecord = createTimeInEvent(cRecord, "0044-03-14 0900")
+        updatedBpRecord = createTimeOutEvent(cRecord, "0044-03-14 2100")
+        // Earns 54
+        updatedBpRecord = createTimeInEvent(cRecord, "0044-03-15 0900")
+        updatedBpRecord = createTimeOutEvent(cRecord, "0044-03-15 1100")
+        // 324 + 54
 
-// }
+function allWagesFor(record) {
+    // grab all dates that employee worked
+    let timeWorked = record.timeInEvents.map(time => time.date)
+    let timeReduce = timeWorked.reduce(function(accumulator, date) {
+       return accumulator + wagesEarnedOnDate(record, date)
+    }, 0)
+    // debugger
+    // take all dates and total wages earned on those dates
+    return timeReduce
+}
 
-// function findEmployeeByFirstName(array, firstName) {
+function findEmployeeByFirstName(array, firstName) {
+    let findEmployee = array.find(name => {
+        return name.firstName === firstName
+    })
+    return findEmployee
+}
 
-// }
+function calculatePayroll(record) {
+    let timeReducer = record.reduce(function(accumulator, record) {
+       return accumulator + allWagesFor(record)
+    }, 0)
+    // debugger
+    return timeReducer
 
-// function calculatePayroll(array) {
-
-// }
+}
